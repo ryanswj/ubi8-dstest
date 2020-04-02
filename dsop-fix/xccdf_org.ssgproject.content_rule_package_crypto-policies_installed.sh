3905fa0470b8e98e339964858b649f23bc1e37ab
Taylor Biggs <taylor@redhat.com>
2020-04-02 23:54:50 +0000

    Update ib repo

--- dsop-fix/xccdf_org.ssgproject.content_rule_package_crypto-policies_installed.sh
+++ dsop-fix/xccdf_org.ssgproject.content_rule_package_crypto-policies_installed.sh
@@ -4,5 +4,5 @@ set -e
 (>&2 echo "Remediating: 'xccdf_org.ssgproject.content_rule_package_crypto-policies_installed'")
 
 if ! rpm -q --quiet "crypto-policies" ; then
-    dnf install -y --disablerepo='*' --enablerepo=ubi-8-appstream-ib,ubi-8-baseos-ib,ubi-8-codeready-builder-ib "crypto-policies"
+    dnf install -y "crypto-policies"
 fi
--- dsop-fix/xccdf_org.ssgproject.content_rule_package_iptables_installed.sh
+++ dsop-fix/xccdf_org.ssgproject.content_rule_package_iptables_installed.sh
@@ -4,5 +4,5 @@ set -e
 (>&2 echo "Remediating: 'xccdf_org.ssgproject.content_rule_package_iptables_installed'")
 
 if ! rpm -q --quiet "iptables" ; then
-    dnf install -y --disablerepo='*' --enablerepo=ubi-8-appstream-ib,ubi-8-baseos-ib,ubi-8-codeready-builder-ib "iptables"
+    dnf install -y "iptables"
 fi
